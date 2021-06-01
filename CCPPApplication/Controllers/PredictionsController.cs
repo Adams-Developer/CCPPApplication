971fab25028d2bc8425bed57e811965fd9c7196e
Tiffany Adams <tadams03@mail.roosevelt.edu>
2021-06-01 15:52:42 -0500

    Update UI, add machine learning project

---
+++ CCPPApplication/Controllers/PredictionsController.cs
@@ -0,0 +1,34 @@
+﻿using CCPPApplicationML.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCPPApplication.Controllers
+{
+    public class PredictionsController : Controller
+    {
+        public IActionResult Index(ModelInput input)
+        {
+            // Create ML Context
+            MLContext mLContext = new MLContext();
+
+            // Load the model
+            ITransformer mlModel = mLContext.Model.Load(@"..\CCPPApplicationML.Model\MLModel.zip", out var modelInputSchema);
+
+            // Create prediction engine needed to load trained model
+            var predictionEngine = mLContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+
+            // Try model on data to predict fare price
+            ModelOutput result = predictionEngine.Predict(input);
+
+            // Predicted Fare in UI
+            ViewBag.FarePrice = result.Score;
+
+            return View(input);
+        }
+
+    }
+}
--- CCPPApplication/Startup.cs
+++ CCPPApplication/Startup.cs
@@ -53,7 +53,7 @@ namespace CCPPApplication
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Predictions}/{action=Index}/{id?}");
             });
         }
     }
